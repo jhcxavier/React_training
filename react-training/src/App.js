75fc85e2f9855bc2f694672ff35a92dd05dc33f1
unknown <luizpaulo.redes@gmail.com>
2020-12-28 16:08:14 -0300

    listofnames created

--- react-training/src/App.js
+++ react-training/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 // import logo from './logo.svg';
 import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
@@ -7,8 +7,18 @@ import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 
 const Validation=()=>{
-  const [country, setCountry]=useState(false)
+
+  
+  
+  const [firstname,setFirstname] = useState('')
+  const [listOfNames,setListOfNames] = useState([])
+  
+  useEffect(()=>{
+    
+  }, [firstname])
+
   return (
+    <span>
     <Paper elevation={3}
       style={{
         borderRadius: '4px',
@@ -23,7 +33,8 @@ const Validation=()=>{
             variant="outlined" 
             size="small" 
             label="First Name" 
-            defaultValue={''}
+            defaultValue={firstname}
+            onChange={ (e)=> {setFirstname(e.target.value)}}
             InputLabelProps={{
               shrink:true
             }}
@@ -56,8 +67,7 @@ const Validation=()=>{
         
         <Box padding={1}>
           <TextField
-            required 
-            error={country}
+            required             
             variant="outlined" 
             size="small" 
             label="Country" 
@@ -72,11 +82,33 @@ const Validation=()=>{
               <Button variant='contained' size='small' style={{marginRight:'5px', color:'blue', backgroundColor:'white', border:'1px solid blue'}}>
                 Cancel
               </Button>
-              <Button variant='contained' size='small' style={{marginRight:'5px', color:'blue', backgroundColor:'white', border:'1px solid blue'}}>
+              <Button onClick={()=> { 
+                setListOfNames([...listOfNames,firstname])
+                console.log(listOfNames)
+              }}
+               variant='contained' size='small' style={{marginRight:'5px', color:'blue', backgroundColor:'white', border:'1px solid blue'}}>
                 Create
               </Button>
             </Box>
     </Paper>
+    <Paper elevation={3}
+      style={{
+        borderRadius: '4px',
+        marginBottom: '15px',
+        paddingBottom: '10px',
+        marginTop:'10px'
+      }}>
+
+        <Grid container>
+          <Box>
+            {listOfNames && listOfNames.map((e,index)=> (
+              <h3 key={index} >{e}</h3>
+              
+            ))}
+          </Box> 
+        </Grid>
+    </Paper>    
+    </span>
   )
 }
 
