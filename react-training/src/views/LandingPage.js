7454c01dd864a0304da500e7f2fa968ba5636b1f
João Henrique Xavier <jhcxavier@gmail.com>
2020-12-29 13:39:39 -0800

    creating landing page

---
+++ react-training/src/views/LandingPage.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+const LandingPage = () => {
+  return (
+    <Container maxWidth="md">
+      <Box display="flex" justifyContent="center">
+        <h1>Contact Management</h1>
+      </Box>
+      <Box
+        display="flex"
+        justifyContent="flex-end"
+        style={{ paddingRight: "10px" }}
+      >
+        <Button
+          variant="contained"
+          size="small"
+          style={{
+            marginRight: "20px",
+            color: "blue",
+            backgroundColor: "white",
+            border: "1px solid blue",
+          }}
+        >
+          Create
+        </Button>
+      </Box>
+    </Container>
+  );
+};
+export default LandingPage;
