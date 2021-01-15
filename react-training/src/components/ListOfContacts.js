7cc66368f8a2fbe87f4dd8a74b98887257a3e5c0
unknown <luizpaulo.redes@gmail.com>
2021-01-15 16:30:25 -0300

    list of contacts rendered on a Material UI table

--- react-training/src/components/ListOfContacts.js
+++ react-training/src/components/ListOfContacts.js
@@ -3,6 +3,14 @@ import { Context } from "../store/appContext";
 import Box from "@material-ui/core/Box";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
 
 const ListOfContacts = () => {
   const { store } = useContext(Context);
@@ -18,20 +26,35 @@ const ListOfContacts = () => {
       }}
     >
       <Grid container>
-        {listOfContacts &&
-          listOfContacts.map((e, index) => (
-            <Box key={index}>
-              <span>
-                {e.firstName +
-                  " " +
-                  e.lastName +
-                  " " +
-                  e.email +
-                  " " +
-                  e.phoneNumber}{" "}
-              </span>
-            </Box>
-          ))}
+
+      <TableContainer component={Paper}>
+                <Table  size="small" aria-label="a dense table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>First Name</TableCell>
+                      <TableCell align="right">Last Name</TableCell>
+                      <TableCell align="right">E-mail</TableCell>
+                      <TableCell align="right">Phone Number&nbsp;</TableCell>                      
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+
+                    {listOfContacts.map((e, index) => (
+                      <TableRow key={index}>
+                        <TableCell component="th" scope="row">
+                          {e.firstName}
+                        </TableCell>
+                        <TableCell align="right">{e.lastName}</TableCell>
+                        <TableCell align="right">{e.email}</TableCell>
+                        <TableCell align="right">{e.phoneNumber}</TableCell>
+                        
+                      </TableRow>
+                    ))}
+                  
+                  </TableBody>
+                </Table>
+              </TableContainer>
+        
       </Grid>
     </Paper>
   );
